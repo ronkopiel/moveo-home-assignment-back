c3a990657c62613de484d6356acc1731eb36b337
ronkopiel <ron.kopiel@gmail.com>
2023-04-25 14:33:17 +0300

    Update codeBlocksRouter.ts

--- src/routes/codeBlocksRouter.ts
+++ src/routes/codeBlocksRouter.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import { getCodeBlock, getCodeBlocks, updateCode } from '../controllers/codeBlockController';
+import { getCodeBlock, getCodeBlocks,  } from '../controllers/codeBlockController';
 
 const router = express.Router();
 router.get('/', getCodeBlocks);
 router.get('/:title', getCodeBlock);
-router.put('/:title', updateCode);
+
 
 export default router;
